9987318c44caedba35c3d8a0291612118696db72
joaocosta1987 <57044291+joaocosta1987@users.noreply.github.com>
2019-10-31 18:05:17 -0300

    formulario fix
    fix formulario

--- AppAvaliacaoDesempenho/src/app/app.module.ts
+++ AppAvaliacaoDesempenho/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
 
 import { Autenticacao } from './autenticacao.service'
 import { AppRoutingModule } from './app-routing.module';
@@ -24,7 +25,8 @@ import { FormularioAvaliacaoComponent } from './formulario-avaliacao/formulario-
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [ Autenticacao ],
   bootstrap: [AppComponent]
